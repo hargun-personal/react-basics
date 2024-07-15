523571833418f9c05fff2d09ab2f84c9fb958905
Hargun Singh <hargun.singh@cartus.com>
2024-07-15 10:30:08 +0530

    understanding JSX

---
+++ Notes.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+/**
+ * 
+ * <div id="parent">
+ *  <div id="child1">
+ *      <h1 id="heading1">Hello World Nested</h1>
+ *      <h2 id="heading2">Nested h2 tag!</h1>
+ *  </div>
+ *  <div id="child2">
+ *      <h1 id="heading1">Hello World Nested</h1>
+ *      <h2 id="heading2">Nested h2 tag!</h1>
+ *  </div>
+ * </div>
+ */
+const heading1 = React.createElement('h1', {id: 'heading'}, 'Hello World Nested');
+const heading2 = React.createElement('h2', {id: 'heading2'}, 'Nested h2 tag!')
+const child1 = React.createElement('div', {id: 'child1'}, [heading1, heading2]);
+const child2 = React.createElement('div', {id: 'child2'}, [heading1, heading2]);
+const parent = React.createElement("div", {id: 'parent'}, [child1, child2] );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+console.log(root);
+root.render(parent);
+root2.render(React.createElement('h3',{id: 'heading3'}, 'This is h3 tag by Root2'));
--- app.js
+++ app.js
@@ -1,36 +1,34 @@
 import React from "react";
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 
-/**
- * 
- * <div id="parent">
- *  <div id="child1">
- *      <h1 id="heading1">Hello World Nested</h1>
- *      <h2 id="heading2">Nested h2 tag!</h1>
- *  </div>
- *  <div id="child2">
- *      <h1 id="heading1">Hello World Nested</h1>
- *      <h2 id="heading2">Nested h2 tag!</h1>
- *  </div>
- * </div>
- */
-const heading1 = React.createElement('h1', {id: 'heading'}, 'Hello World Nested');
-const heading2 = React.createElement('h2', {id: 'heading2'}, 'Nested h2 tag!')
-const child1 = React.createElement('div', {id: 'child1'}, [heading1, heading2]);
-const child2 = React.createElement('div', {id: 'child2'}, [heading1, heading2]);
-const parent = React.createElement("div", {id: 'parent'}, [child1, child2] );
+const heading = React.createElement(
+  "h1",
+  { id: "heading", aer: "abc" },
+  "Hello World By React!"
+);
+const number = 1001;
 
-// console.log(parent);
+const JSXHeading = ( 
+    <h1 id="heading" className="head" tabIndex="5">
+        Hello by JSX {number *18}
+    </h1>
+)
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", aer: "abc" },
-//   "Hello World By React!"
-// );
+const TitleComponent = () => {
+    return <h2>TitleComponent</h2>
+}
+
+// React Functional Componnet
+
+const HeadingComponet = () => (
+    <div id="container">
+    {JSXHeading}
+    {TitleComponent()}
+    <TitleComponent/>
+    <TitleComponent></TitleComponent>
+        <h1 className="heading" >This is Functional component</h1>
+    </div> 
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const root2 = ReactDOM.createRoot(document.getElementById("root2"));
-console.log(root);
-root.render(parent);
-root2.render(React.createElement('h3',{id: 'heading3'}, 'This is h3 tag by Root2'))
-console.log(root.render(parent));
+root.render(<HeadingComponet/>);
